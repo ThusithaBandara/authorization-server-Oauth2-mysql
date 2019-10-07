bd261a35e4f4d0d609c78caecd32e58ceeb19e49
ThusithaBandara <thusitharmtgk95@gmail.com>
2019-10-07 17:06:55 +0530

    oauth2 server okay,need to store token in db

---
+++ src/main/java/com/krishantha/rentcloud/authorizationserver/model/Permission.java
@@ -0,0 +1,54 @@
+package com.krishantha.rentcloud.authorizationserver.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "permission")
+public class Permission {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+	
+	@Column(name = "name")
+	private String name;
+
+	/**
+	 * @param id
+	 * @param name
+	 */
+	public Permission() {
+		
+	}
+	
+	public Permission(Integer id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+
+}
